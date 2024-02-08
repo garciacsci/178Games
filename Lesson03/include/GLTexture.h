64cdd83cb1adeed1e5501bd41e770fc92da02943
garciacce23 <62458331+garciacce23@users.noreply.github.com>
2024-02-07 17:13:59 -0800

    Lesson 03

---
+++ Lesson03/include/GLTexture.h
@@ -0,0 +1,28 @@
+#ifndef GLTEXTURE_H
+#define GLTEXTURE_H
+
+#include <SOIL.h>
+#include <gl/gl.h>
+
+#include <iostream>
+
+using namespace std;
+
+class GLTexture
+{
+    public:
+        GLTexture();
+        virtual ~GLTexture();
+        GLuint tex;
+        GLint width,height;
+        unsigned char* image;
+        void loadTexture(char *);
+        void bindTexture();
+
+
+    protected:
+
+    private:
+};
+
+#endif // GLTEXTURE_H
